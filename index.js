d8ce6501c92e54af87a54849aa52b18d989e3fd2
Vinicius Cole de Amorim <viniamorim87@hotmail.com>
2023-08-23 20:19:46 -0300

    enabling cors

--- index.js
+++ index.js
@@ -1,11 +1,13 @@
 const express = require('express');
 const app = express();
 const connectTODB = require('./database/db');
+const cors = require('cors');
 
 connectTODB();
 const port = 3000;
 const routes = require('./routes/routes');
 
+app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(routes);
